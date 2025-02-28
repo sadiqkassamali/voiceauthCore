b88995e688bf3f38ef99617498df71df58e2e712
sadiq kassamali <Sadiq kassamali@gmail.com>
2025-02-28 12:03:57 -0600

    Rearrange

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="voiceauthCore",
-    version="0.6",
+    version="0.6.5",
     author="sadiq kassamali",
     author_email="sadiq.kasssamali@gmail.com",
     description="A deepfake audio detection tool",
     long_description=open('README.md',encoding="utf8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/sadiqkassamali/voiceauthCore",
-    packages=find_packages(),
+    ackages=find_packages('.'),
     install_requires=[
         "tensorflow", "librosa", "pydub", "numpy", "scipy", "transformers" , "torch", "pillow"
     ],
