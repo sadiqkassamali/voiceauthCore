f65afadf55cda2ec2d275ff9669688b3e50b5b52
sadiq kassamali <Sadiq kassamali@gmail.com>
2025-03-01 01:23:08 -0600

    if condition for images or audio

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except FileNotFoundError:
 
 setup(
     name="voiceauthCore",
-    version="0.1.6",
+    version="0.1.7",
     author="Sadiq Kassamali",
     author_email="sadiq.kasssamali@gmail.com",
     description="A deepfake audio detection tool",
